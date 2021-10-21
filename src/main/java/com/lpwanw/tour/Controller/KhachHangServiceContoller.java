be4beb0f5bf3d36a06b96f161b589dfe9085f242
lpwanw <phuongtay2000@gmail.com>
2021-10-21 20:50:33 +0700

    update khachhangService

--- src/main/java/com/lpwanw/tour/Controller/KhachHangServiceContoller.java
+++ src/main/java/com/lpwanw/tour/Controller/KhachHangServiceContoller.java
@@ -3,21 +3,50 @@ package com.lpwanw.tour.Controller;
 import com.lpwanw.tour.Entity.KhachhangEntity;
 import com.lpwanw.tour.Reponsitoty.KhachhangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/khachhang")
+@RequestMapping("/api/khachhangs")
 public class KhachHangServiceContoller {
     @Autowired
     KhachhangRepository khachhangRepository;
-    @PostMapping("/add")
+    @PostMapping("")
     public KhachhangEntity addOne(@RequestBody KhachhangEntity khachhangEntity){
-        return khachhangRepository.save(khachhangEntity);
+        int id = khachhangRepository.save(khachhangEntity).getId();
+        return khachhangRepository.getById(id);
     }
-    @GetMapping("/get/{id}")
-    public KhachhangEntity getById(@PathVariable int id){
-        return  khachhangRepository.findById(id).get();
+    @GetMapping("/{id}")
+    public ResponseEntity<KhachhangEntity> getById(@PathVariable int id){
+        Optional<KhachhangEntity> kh = khachhangRepository.findById(id);
+        return kh.isPresent()?new ResponseEntity<>(kh.get(), HttpStatus.OK):new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @GetMapping("")
+    public List<KhachhangEntity> getAll(){
+        return khachhangRepository.findAll();
+    }
+    @PutMapping("")
+    public ResponseEntity<KhachhangEntity> update(@RequestBody KhachhangEntity khachhangEntity){
+        Optional<KhachhangEntity> kh = khachhangRepository.findById(khachhangEntity.getId());
+        if(kh.isPresent()){
+            return new ResponseEntity<>(khachhangRepository.save(khachhangEntity), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<KhachhangEntity> delete(@PathVariable int id){
+        Optional<KhachhangEntity> kh = khachhangRepository.findById(id);
+        if(kh.isPresent()){
+            khachhangRepository.delete(kh.get());
+            return new ResponseEntity<>(kh.get(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/lpwanw/tour/Entity/KhachhangEntity.java
+++ src/main/java/com/lpwanw/tour/Entity/KhachhangEntity.java
@@ -1,10 +1,13 @@
 package com.lpwanw.tour.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Date;
 
 @Table(name = "tour_khachhang")
 @Entity
@@ -23,6 +26,7 @@ public class KhachhangEntity {
     private String khSdt;
 
     @Column(name = "kh_ngaysinh")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate khNgaysinh;
 
     @Lob
