2df838ae26971a86f1dc45a2a356e8d33700e09f
NgocMinhnn <61500935+NgocMinhnn@users.noreply.github.com>
2021-10-22 20:16:26 +0700

    tour feature

--- src/main/java/com/lpwanw/tour/DTO/TourDTO.java
+++ src/main/java/com/lpwanw/tour/DTO/TourDTO.java
@@ -1,5 +1,49 @@
 package com.lpwanw.tour.DTO;
 
 public class TourDTO {
+    private Integer id;
+    private String tourTen;
+    private String tourMoTa;
+    private String tenLoai;
+    private String moTaLoai;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTourTen() {
+        return tourTen;
+    }
+
+    public void setTourTen(String tourTen) {
+        this.tourTen = tourTen;
+    }
+
+    public String getTourMoTa() {
+        return tourMoTa;
+    }
+
+    public void setTourMoTa(String tourMoTa) {
+        this.tourMoTa = tourMoTa;
+    }
+
+    public String getTenLoai() {
+        return tenLoai;
+    }
+
+    public void setTenLoai(String tenLoai) {
+        this.tenLoai = tenLoai;
+    }
+
+    public String getMoTaLoai() {
+        return moTaLoai;
+    }
+
+    public void setMoTaLoai(String moTaLoai) {
+        this.moTaLoai = moTaLoai;
+    }
 }
